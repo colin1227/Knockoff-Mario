df4e7183249b3047106692a38cb304ee0d82d59f
Colin Daniel <colinmdaniel@gmail.com>
2018-09-30 11:12:01 -0500

    collision really works now going to make it more versitile

--- js/tut.js
+++ js/tut.js
@@ -10,11 +10,8 @@ let direction = {
 
 let fireCounter = 0;
 
-let gravity = 1;
- let theX = 400;
- let theX2 = window.innerWidth;
- let theY =  310;
- let theY2 = 390;
+let gravity = 3;
+
 ///////////////////////////////////////////////////////////////////
 const movingLeft = () =>{
 	console.log("moving left");
@@ -66,12 +63,28 @@ canvas.height = window.innerHeight;
 
 window.addEventListener("resize", resizer)
 
+class Ground{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+	}
+}
+class Block {
+	constructor(x,y,width,height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height
+	}
+}
 
 
 class ThePlayer {
-	constructor(x, y, dx, dy, health, ammo, win){
+	constructor(x, y, width, height, dx, dy, health, ammo, win){
 		this.x = x;
 		this.y = y;
+		this.width = width;
+		this.height = height;
 		this.dx = dx;
 		this.dy = dy;
 		this.health = health;
@@ -81,65 +94,67 @@ class ThePlayer {
 	}
 	 animating (){
 		
-		 if (direction.right == true && direction.up == true && theY < 390 - 80){
-			 theX--;
-			 theX2++;
+		 if (direction.right == true && direction.up == true && aBlock.y < 390 - 80){
+			 aBlock.x--;
+			 tutG.x++;
 			theY -= 3;
-			 theY2 += 3;
+			 tutG.y += 3;
 	   }
-       else if(direction.right == true && theX > 115 ){
-     	 theX--;
- 	     theX2++;
+       else if(direction.right == true && aBlock.x > 115 ){
+     	 aBlock.x-= first.dx;
+ 	     tutG.x-= first.dx;
  }
-		 else if (direction.right && direction.up == false && theY < 390 - 80){
+		 else if (direction.right && direction.up == false && aBlock.y < 390 - 80){
 			if(theX > 76) {
-			theX--;
-			 theX2++;
+			aBlock.x--;
+			 tutG.x++;
 			 theY += gravity;
-			 theY2 -= gravity;
+			 tutG.y -= gravity;
 		 }
 	   }
 		 else if (direction.left == true){
-     	 theX++;
-     	 theX2--;
+     	 aBlock.x++;
+     	 tutG.x--;
  } 
-		 else if (direction.down == true && theY < 390 - 80){	
- 	     theY++;
- 	     theY2--;
+		 else if (direction.down == true && aBlock.y > 390 - 80){	
+ 	     aBlock.y--;
+ 	     tutG.y--;
  }
 		 else if (direction.up == true){
- 	     theY-= 3;
-		  theY2+=3;
+ 	     aBlock.y+= 3;
+		  tutG.y+=3;
 	   }
-       else if(direction.up == false && theY < 390 - 80){
-		 theY+= gravity;
-		 theY2 -= gravity;
-
-
-}
- else{
- 	theX += 0;
- 	theX2 += 0;
- }
+       else {
+		// aBlock.y-= gravity;
+		if (aBlock.y > 390 - 40){
+		 aBlock.y-= gravity;
+		 tutG.y -= gravity;
+			 }
+		  aBlock.x += 0;
+		  tutG.x += 0;
+	 }
 
  //player
 c.fillStyle = '#00FF0F';
-c.fillRect(75, theY, 40, 80)
+c.fillRect(first.x, first.y, 40, 80)
 
 c.fillStyle ='#FF0000';
-c.fillRect(theX, theY2 - 40, 40, 40);
+c.fillRect(aBlock.x, aBlock.y, aBlock.width, aBlock.height);
 c.beginPath();
-c.moveTo(0, theY2);
-c.lineTo( theX2, theY2);
+c.moveTo(0, tutG.y);
+c.lineTo( tutG.x, tutG.y);
 c.strokeStyle = "blue";
 c.stroke();
 	 }
 	}
-
 	let first;
+    let aBlock;
+    let tutG;
 	const init  = () => {
-	first = new ThePlayer(3,3,3,3,3,3,false)
-	first.animating();
+	    first = new ThePlayer(75,310,40,80,5,3,3,3,false);
+        aBlock = new Block(400, 350, 40, 40);
+        tutG = new Ground(window.innerWidth, 390);
+	    first.animating();
 
 	}
 
@@ -168,7 +183,7 @@ function getDistance (x1, y1, x2, y2){
 
 /////////////////////////////////////////////////////////////////////
 
-
+// the funcion callbacks based on which key codes are pressed
 $(document).on("keydown", (e) =>{
 	if (event.keyCode == 39) {
 		direction.right = true;
@@ -186,11 +201,14 @@ $(document).on("keydown", (e) =>{
 		direction.down = true;
 		movingDown();
 	}
+// the function call that fires bullets
 	else if(event.keyCode == 32){
 		fire();
 		
 	}
 }).on("keyup", (e)=>{
+
+	//resets direction object
  if(event.keyCode == 39 ){  
 console.log("stopped moving right");
  direction.right = false;
@@ -207,6 +225,8 @@ else if(event.keyCode == 40){
 	console.log("stopped moving down");
 	direction.down = false;
 }
+
+  //resets firing mechanic
 else if(event.keyCode == 32){
 console.log("stopped firing");
 fireCounter = 0;
@@ -225,4 +245,7 @@ fireCounter = 0;
 
 
 
-
+// let theX = 400;
+//let theX2 = window.innerWidth;
+// let theY = 310;
+//let theY2 = 390;
